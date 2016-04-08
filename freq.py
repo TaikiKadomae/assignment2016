6208ea0a4c16cb3b02b910a5ae8b7925fb7ded30
Taikikadomae <koppepann660@gmail.com>
2016-04-08 14:15:32 +0900

    assignment2.3-9

---
+++ freq.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import sys
+import re
+import difflib
+from collections import OrderedDict
+
+#コマンドライン引数使用
+argvs = sys.argv
+argc = len(argvs)
+
+#引数が足りない場合はメッセージ表示
+if (argc == 1):
+	print('Usage: # python %s filename' % argvs[0])
+	quit()
+
+#辞書の中に単語があれば値を+1
+#なければ辞書に追加
+count = OrderedDict()
+for line in open(argvs[1], 'r'):
+	lene = line.strip('\n')
+	sp = re.split(' +', line)
+	for i in range(len(sp)):
+		if sp[i] in count:
+			count[sp[i]] += 1
+		else:
+			count[sp[i]] = 1
+
+#辞書から改行を削除
+del count["\n"]
+
+#辞書を値順にソートして表示
+countOfLoop = 0
+top10 = []
+for k, v in sorted(count.items(), key = lambda x:x[1]):
+	countOfLoop += 1
+	if (countOfLoop > len(count) - 10):
+		top10.append(str(v) + " " + k)
+
+#比較する
+if (argc == 3):
+	diff = difflib.unified_diff(top10, open(argvs[2], 'r').read().strip().split('\n'))
+	for j in diff:
+		print(j)

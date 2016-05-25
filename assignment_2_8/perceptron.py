0b9b90fad4d59a0eff2ff3c05c66387abd1a3101
Taikikadomae <koppepann660@gmail.com>
2016-05-25 15:16:47 +0900

    commit

--- assignment_2_8/perceptron.py
+++ assignment_2_8/perceptron.py
@@ -9,83 +9,70 @@ def read_instance(s):
 	for j in splitLine:
 		if (j != splitLine[0]):
 			tup = j.split(':')
-			tupleList.append(tuple(tup))
+			tupleList.append((tup[0],tup[1]))
 	fvAndLabel = (splitLine[0], tupleList)
 	return fvAndLabel
 
 def read_data(train,test):
-	line = open(train).readlines()
-	line2 = open(test).readlines()
 	dataList = []
 	tdataList = []
 	indexList = []
-	for i in line:
+	for i in open(train).readlines():
 		tup = read_instance(i)
 		dataList.append(tup)
 
 	for j in dataList:
 		indexList.append(int(j[1][len(j[1])-1][0]))
 
-	for k in line2:
+	for k in open(test).readlines():
 		tup2 = read_instance(k)
 		tdataList.append(tup2)
 		
 	max_index = max(indexList)
 	return (dataList, tdataList, max_index)
 
-def add_fv(fv, weight):
+def add_fv(fv):
 	for i in fv[1]:
 		weight[int(i[0])] += int(i[1])
-	return weight
 
-def sub_fv(fv, weight):
+def sub_fv(fv):
 	for i in fv[1]:
 		weight[int(i[0])] -= int(i[1])
-	return weight
 
-def mult(fv, weight):
+def mult(fv):
 	sumdp = 0
 	for i in fv[1]:
-		if int(i[0]) < len(weight):
+		if (int(i[0]) < len(weight)):
 			sumdp += int(i[1]) * weight[int(i[0])]
-		
 	return sumdp
 
-def update_weight(data, weight):
-	w = weight
+def update_weight(data):
 	for ins in data:
-		if ins == data[0]:
-			if int(ins[0]) > 0:
-				w = add_fv(ins, w)
+		if (ins == data[0]):
+			if (int(ins[0]) > 0):
+				add_fv(ins)
 			else:
-				w = sub_fv(ins, w)
+				sub_fv(ins)
 		else:
-			if int(ins[0]) * mult(ins, w) < 0:
-				if int(ins[0]) > 0:
-					w = add_fv(ins, w)
-				elif int(ins[0]) < 0:
-					w = sub_fv(ins, w)
-				else:
-					pass
-			else:
-				pass
+			if (int(ins[0]) * mult(ins) <= 0):
+				if (ins[0] == '1'):
+					add_fv(ins)
+				elif (ins[0] == '-1'):
+					sub_fv(ins)
 				
-	return w
-def evaluate(test_data, w):
+def evaluate(test_data):
 	count = 0
 	inscount = 0
 	for ins in test_data:
 		inscount += 1
-		if int(ins[0]) * mult(ins, w) > 0:
+		if (int(ins[0]) * mult(ins) > 0):
 			count += 1
 	return(count, inscount, count/inscount)
 	
 if __name__ == "__main__":
 	train_data, test_data, max_index = read_data(sys.argv[1], sys.argv[2])
-
 	weight = [0] * (max_index + 1)
 	for i in range(int(sys.argv[3])):
-		weight = update_weight(train_data, weight)
-	correct, instance, rate = evaluate(test_data, weight)
-	print(correct, instance, rate)
-	
+		update_weight(train_data)
+	correct, instance, rate = evaluate(test_data)
+	print(correct, instance, str(rate*100) + '%')

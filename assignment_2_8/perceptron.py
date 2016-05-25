20de5993b0bf0169b51b31099cc374f11e7b8644
Taikikadomae <koppepann660@gmail.com>
2016-05-25 14:35:47 +0900

    commit

--- assignment_2_8/perceptron.py
+++ assignment_2_8/perceptron.py
@@ -13,40 +13,79 @@ def read_instance(s):
 	fvAndLabel = (splitLine[0], tupleList)
 	return fvAndLabel
 
-def read_data(s):
-	line = open(s).readlines()
+def read_data(train,test):
+	line = open(train).readlines()
+	line2 = open(test).readlines()
 	dataList = []
-	lenList = []
+	tdataList = []
+	indexList = []
 	for i in line:
 		tup = read_instance(i)
 		dataList.append(tup)
-		lenList.append(len(tup[1]))
-	return (dataList, max(lenList))
 
-def add_fv(fv):
-	zero = [0] * (len(weight) - len(fv))
-	npfv = np.array(fv.extend(zero))
-	npw = np.array(weight)
-	return npw + npfv
+	for j in dataList:
+		indexList.append(int(j[1][len(j[1])-1][0]))
 
-def sub_fv(fv):
-	zero = [0] * (len(weight) - len(fv))
-	npfv = np.array(fv.extand(zero))
-	npw = np.array(weight)
-	return npw - npfv
-
-def mult(fv):
-	if not len(weight) < len(fv)
-		zero = [0] *(len(weight) - len(fv)) 
-		npfv = np.array(fv.extend(zero))
-		npw = n.array(weight)
-		return sum(npw * npfv)
-
-def update_weight(data):
-	for i in data:
+	for k in line2:
+		tup2 = read_instance(k)
+		tdataList.append(tup2)
 		
+	max_index = max(indexList)
+	return (dataList, tdataList, max_index)
 
+def add_fv(fv, weight):
+	for i in fv[1]:
+		weight[int(i[0])] += int(i[1])
+	return weight
+
+def sub_fv(fv, weight):
+	for i in fv[1]:
+		weight[int(i[0])] -= int(i[1])
+	return weight
+
+def mult(fv, weight):
+	sumdp = 0
+	for i in fv[1]:
+		if int(i[0]) < len(weight):
+			sumdp += int(i[1]) * weight[int(i[0])]
+		
+	return sumdp
+
+def update_weight(data, weight):
+	w = weight
+	for ins in data:
+		if ins == data[0]:
+			if int(ins[0]) > 0:
+				w = add_fv(ins, w)
+			else:
+				w = sub_fv(ins, w)
+		else:
+			if int(ins[0]) * mult(ins, w) < 0:
+				if int(ins[0]) > 0:
+					w = add_fv(ins, w)
+				elif int(ins[0]) < 0:
+					w = sub_fv(ins, w)
+				else:
+					pass
+			else:
+				pass
+				
+	return w
+def evaluate(test_data, w):
+	count = 0
+	inscount = 0
+	for ins in test_data:
+		inscount += 1
+		if int(ins[0]) * mult(ins, w) > 0:
+			count += 1
+	return(count, inscount, count/inscount)
+	
 if __name__ == "__main__":
-	train_data, max_index = read_data(sys.argv[1])
-	weight =[0] * (max_index + 1)
+	train_data, test_data, max_index = read_data(sys.argv[1], sys.argv[2])
+
+	weight = [0] * (max_index + 1)
+	for i in range(int(sys.argv[3])):
+		weight = update_weight(train_data, weight)
+	correct, instance, rate = evaluate(test_data, weight)
+	print(correct, instance, rate)
 	

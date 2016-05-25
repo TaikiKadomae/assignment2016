012bc8c3a1dbd31322e30ce25bfab01e149cf424
Taikikadomae <koppepann660@gmail.com>
2016-05-25 15:27:33 +0900

    commit

--- assignment_2_8/perceptron.py
+++ assignment_2_8/perceptron.py
@@ -2,15 +2,15 @@
 #-*- coding:UTF-8 -*-
 
 import sys
-import numpy as np
+
 def read_instance(s):
 	tupleList = []
 	splitLine = s.strip().split(' ')
 	for j in splitLine:
 		if (j != splitLine[0]):
 			tup = j.split(':')
-			tupleList.append((tup[0],tup[1]))
-	fvAndLabel = (splitLine[0], tupleList)
+			tupleList.append((int(tup[0]),int(tup[1])))
+	fvAndLabel = (int(splitLine[0]), tupleList)
 	return fvAndLabel
 
 def read_data(train,test):
@@ -22,7 +22,7 @@ def read_data(train,test):
 		dataList.append(tup)
 
 	for j in dataList:
-		indexList.append(int(j[1][len(j[1])-1][0]))
+		indexList.append(j[1][len(j[1])-1][0])
 
 	for k in open(test).readlines():
 		tup2 = read_instance(k)
@@ -33,31 +33,31 @@ def read_data(train,test):
 
 def add_fv(fv):
 	for i in fv[1]:
-		weight[int(i[0])] += int(i[1])
+		weight[i[0]] += i[1]
 
 def sub_fv(fv):
 	for i in fv[1]:
-		weight[int(i[0])] -= int(i[1])
+		weight[i[0]] -= i[1]
 
 def mult(fv):
 	sumdp = 0
 	for i in fv[1]:
-		if (int(i[0]) < len(weight)):
-			sumdp += int(i[1]) * weight[int(i[0])]
+		if (i[0] < len(weight)):
+			sumdp += i[1] * weight[i[0]]
 	return sumdp
 
 def update_weight(data):
 	for ins in data:
 		if (ins == data[0]):
-			if (int(ins[0]) > 0):
+			if (ins[0] > 0):
 				add_fv(ins)
 			else:
 				sub_fv(ins)
 		else:
-			if (int(ins[0]) * mult(ins) <= 0):
-				if (ins[0] == '1'):
+			if (ins[0] * mult(ins) <= 0):
+				if (ins[0] == 1):
 					add_fv(ins)
-				elif (ins[0] == '-1'):
+				elif (ins[0] == -1):
 					sub_fv(ins)
 				
 def evaluate(test_data):
@@ -65,7 +65,7 @@ def evaluate(test_data):
 	inscount = 0
 	for ins in test_data:
 		inscount += 1
-		if (int(ins[0]) * mult(ins) > 0):
+		if (ins[0] * mult(ins) > 0):
 			count += 1
 	return(count, inscount, count/inscount)
 	
